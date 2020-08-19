ded7558677f3a8c58d6f59a6de2c799423cfd3f8
yuya3009 <yoko3057co@gmail.com>
2020-08-19 06:44:46 +0000

    Finish make-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
+  get '/active', to: 'static_pages#active'
+  get '/signup', to: 'users#new'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,15 +3,21 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "TRIPO_app"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | TRIPO_app"
+  end
+  
+  test "should get active" do
+    get active_path
+    assert_response :success
+    assert_select "title", "Active | TRIPO_app"
   end
 
 end
