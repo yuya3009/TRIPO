03479fc56d3dd479d559e822c27e1b3c751cbc5a
yuya3009 <yoko3057co@gmail.com>
2020-08-26 07:46:01 +0000

    finish session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "アカウントを作成しました！"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/active', to: 'static_pages#active'
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
