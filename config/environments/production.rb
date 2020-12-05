f07c79537d9f5c4108da5cd14fa4f322ecb5553d
yuya3009 <yoko3057co@gmail.com>
2020-12-05 08:01:02 +0000

    smtp_setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,15 +16,15 @@ Rails.application.configure do
   host = 'tricolor-point2020.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-     :port => ENV['587'],
-     :address => ENV['smtp.mailgun.org'],
+     :port => '587',
+     :address => 'smtp.mailgun.org',
      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
      :domain => host,
      :authentication => :plain,
   }
 
-  config.public_file_server.enabled = ENV['enabled'].present?
+  config.public_file_server.enabled = 'enabled'.present?
 
   config.assets.js_compressor = :uglifier
  
